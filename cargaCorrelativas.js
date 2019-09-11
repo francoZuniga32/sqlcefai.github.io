6f0e266821192db772182ecb3bed629d1c42ddd9
francoZuniga32 <54842987+francoZuniga32@users.noreply.github.com>
2019-09-11 13:44:06 -0300

    prueva2

--- cargaCorrelativas.js
+++ cargaCorrelativas.js
@@ -86,7 +86,8 @@ function armar() {
     var materia1 = document.getElementById("materia").value;
     var materia2 = document.getElementById("materia2").value;
     var correlativa = "('"+materia1+"','"+materia2+"'),";
-    document.getElementById("correlativaMuestra").value += correlativa; 
+    console.log(document.getElementById("correlativaMuestra").value);
+    // += correlativa; 
     //document.getElementById("correlativaMuestra").value = armado;
 }
 
