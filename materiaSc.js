c117ab3a910f54bed395cc9551877d074d8a9908
francoZuniga32 <franco.ojeda@est.fi.uncoma.edu.ar>
2019-09-11 23:17:34 -0300

    19w37d1109h2152

---
+++ materiaSc.js
@@ -0,0 +1,17 @@
+
+function armar(){
+    console.log("hola mundo");
+    var materia = document.getElementById("materia").value;
+    var año = document.getElementById("año").value;
+    var cuatrimestre = document.getElementById("cuatrimestre").value;
+
+    var insertado = "(NULL,'"+materia+"',"+año+","+cuatrimestre+"),";
+    var cargado = document.getElementById("materiaMuestra").value;
+    cargado += insertado; 
+    document.getElementById("materiaMuestra").value = cargado;
+}
+
+function descargar(){
+    var valor = document.getElementById("materiaMuestra").value
+    console.log(valor.slice(,valor.length()));
+}
