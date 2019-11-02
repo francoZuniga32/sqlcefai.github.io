c5fd04d96a0431cf29d11767af9eec0b029d18c0
franco <franco.ojeda@est.fi.uncoma.edu.ar>
2019-11-02 16:55:58 -0300

    d211h1655

--- JS/horarios.js
+++ JS/horarios.js
@@ -1,9 +1,11 @@
 function cargar() {
+
     var valueMateria = document.getElementById("materia").value;
     var valueModulo = document.getElementById("modulo").value;
     var valueDia = document.getElementById("dia").value;
     var valueHora = document.getElementById("hora").value;
     var valueAula = document.getElementById("aula").value;
+    var valueCuatrimestre = document.getElementById("cuatrimestre").value;
 
     var tabla = document.getElementById("horarios");
 
@@ -25,16 +27,21 @@ function cargar() {
     //cargamos el modulo
     var modulo = document.createElement('td');
     modulo.innerHTML = valueModulo;
+    //cargamos el cuatrimestre
+    var cuatrimestre = document.createElement('td');
+    modulo.innerHTML = valueCuatrimestre;
+
 
     //cargamos todo
     tr.appendChild(dia);
-    tr.appendChild(aula);
     tr.appendChild(hora);
     tr.appendChild(materia);
+    tr.appendChild(aula);
     tr.appendChild(modulo);
-
+    tr.appendChild(cuatrimestre);
+    
     //cargamos en el textarea
-    var sql = "('"+valueMateria+",'"+valueAula+"','"+valueDia+"','"+valueModulo+"','"+valueHora+"'),";
+    var sql = "('"+valueMateria+",'"+valueAula+"','"+valueDia+"','"+valueModulo+"','"+valueHora+"', '"+valueCuatrimestre+"', 'normal'),";
     var cargado = document.getElementById("muestra").value;
     cargado += sql; 
     document.getElementById("muestra").value = cargado;
